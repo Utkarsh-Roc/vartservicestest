2cdc96ea290cfb69521399520872664545d33862
Utkarsh-Roc <utkarshlotia@gmail.com>
2025-06-11 19:15:24 +0530

    Updated theme of pay now in header

--- src/common/header/HeaderOne.js
+++ src/common/header/HeaderOne.js
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect} from "react";
+import { useState, useRef, useEffect } from "react";
 import { FiMenu } from "react-icons/fi";
 import Logo from "../../elements/logo/Logo";
 import Nav from './Nav';
@@ -7,23 +7,23 @@ import Darkmode from "./Darkmode";
 import useStickyHeader from "./useStickyHeader";
 import { Link } from "react-router-dom";
 
-const HeaderOne = ({btnStyle, HeaderSTyle}) => {
+const HeaderOne = ({ btnStyle, HeaderSTyle }) => {
     const [ofcanvasShow, setOffcanvasShow] = useState(false);
     const [showCurrencyDropdown, setShowCurrencyDropdown] = useState(false);
     const dropdownRef = useRef(null);
-    
+
     const onCanvasHandler = () => {
         setOffcanvasShow(prev => !prev);
     }
-    
+
     const ref = useRef();
     let [check, setCheck] = useState(true);
-    const sticky = useStickyHeader( 50 );
+    const sticky = useStickyHeader(50);
     const headerClasses = `header-default ${(sticky && check) ? 'sticky' : ''}`
     const { clientHeight } = ref;
-    
+
     const checkChange = (value) => {
-      setCheck(value);
+        setCheck(value);
     };
 
     // Close dropdown when clicking outside
@@ -56,7 +56,7 @@ const HeaderOne = ({btnStyle, HeaderSTyle}) => {
                 url = "https://rzp.io/rzp/jobspaymentsusd";
                 break;
             case "EUR":
-                url = "https://rzp.io/rzp/jobspaymenteur"; 
+                url = "https://rzp.io/rzp/jobspaymenteur";
                 break;
             default:
                 return;
@@ -78,7 +78,7 @@ const HeaderOne = ({btnStyle, HeaderSTyle}) => {
                 <div className="container position-relative">
                     <div className="row align-items-center row--0">
                         <div className="col-lg-3 col-md-6 col-4">
-                            <Logo 
+                            <Logo
                                 image={`${process.env.PUBLIC_URL}/images/logo/logo.png`}
                                 image2={`${process.env.PUBLIC_URL}/images/logo/logo-dark.png`}
                             />
@@ -90,62 +90,33 @@ const HeaderOne = ({btnStyle, HeaderSTyle}) => {
                                 </nav>
                                 <div className="header-btn">
                                     <div className="header-payment-dropdown" style={{ position: "relative", display: "inline-block" }} ref={dropdownRef}>
-<button
-    onClick={handleDropdownToggle}
-    className={`btn-default header-pay-btn ${btnStyle}`}
-    style={{
-        padding: "0px 30px 0px 15px",
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        gap: "10px"
-    }}
->
-    {/* Razorpay SVG Icon */}
-    <svg
-        width="18"
-        height="20"
-        viewBox="0 0 18 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path
-            d="M7.077 6.476l-.988 3.569 5.65-3.589-3.695 13.54 3.752.004 5.457-20L7.077 6.476z"
-            fill="#fff"
-        ></path>
-        <path
-            d="M1.455 14.308L0 20h7.202L10.149 8.42l-8.694 5.887z"
-            fill="#fff"
-        ></path>
-    </svg>
+                                        <button
+                                            onClick={handleDropdownToggle}
+                                            className="PaymentButton-Button PaymentButton-Button--rzpTheme currency-dropdown-button header-payment-button">
+                                            <svg className="mysvg" width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M7.077 6.476l-.988 3.569 5.65-3.589-3.695 13.54 3.752.004 5.457-20L7.077 6.476z" fill="#fff"></path>
+                                                <path d="M1.455 14.308L0 20h7.202L10.149 8.42l-8.694 5.887z" fill="#fff"></path>
+                                            </svg>
+                                            <div className="PaymentButton-contents">
+                                                <span className="PaymentButton-text">Pay Now</span>
+                                                <div className="PoweredBy">Secured by Razorpay</div>
+                                            </div>
+                                            <svg
+                                                className="dropdown-chevron"
+                                                width="12"
+                                                height="12"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                style={{
+                                                    transform: showCurrencyDropdown ? "rotate(180deg)" : "rotate(0deg)"
+                                                }}
+                                            >
+                                                <path d="M6 9l6 6 6-6" />
+                                            </svg>
+                                        </button>
 
-    {/* Button Text Content */}
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", lineHeight: 1 }}>
-        <span style={{ fontWeight: 600 }}>Pay Now</span>
-        <div style={{ fontSize: "10px", color: "#ccc" }}>Secured by Razorpay</div>
-    </div>
-
-    {/* Chevron */}
-    <svg
-        width="12"
-        height="12"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        style={{
-            position: "absolute",
-            right: "8px",
-            top: "50%",
-            transform: showCurrencyDropdown
-                ? "translateY(-50%) rotate(180deg)"
-                : "translateY(-50%) rotate(0deg)",
-            transition: "transform 0.2s ease"
-        }}
-    >
-        <path d="M6 9l6 6 6-6" />
-    </svg>
-</button>
 
                                         {showCurrencyDropdown && (
                                             <ul className="header-currency-dropdown">
@@ -184,12 +155,12 @@ const HeaderOne = ({btnStyle, HeaderSTyle}) => {
                                     </div>
                                 </div>
                                 <Darkmode />
-                            </div>  
+                            </div>
                         </div>
                     </div>
                 </div>
             </header>
-            <MobileMenu show={ofcanvasShow} onClose={onCanvasHandler}  />
+            <MobileMenu show={ofcanvasShow} onClose={onCanvasHandler} />
         </>
     )
 }
